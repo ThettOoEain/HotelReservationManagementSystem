cdeac456a38d6f5c5c7e487c7d0ee77453a25642
Thett Oo Eain <thettooeain@gmail.com>
2021-05-20 17:14:21 +0800

    create edit category page

--- HotelReservationManagementSystem/Areas/Admin/Controllers/CategoryController.cs
+++ HotelReservationManagementSystem/Areas/Admin/Controllers/CategoryController.cs
@@ -42,5 +42,35 @@ namespace HotelReservationManagementSystem.Areas.Admin.Controllers
             }
             return View(category);
         }
+
+        //GET-Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        //POST-Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(category);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+
+        }
     }
 }
