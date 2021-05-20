8d410e66735ff85f28e77d39a05deda4901c14db
Thett Oo Eain <thettooeain@gmail.com>
2021-05-20 14:58:40 +0800

    Areas configuration

--- HotelReservationManagementSystem/Areas/Customer/Controllers/HomeController.cs
+++ HotelReservationManagementSystem/Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace HotelReservationManagementSystem.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
